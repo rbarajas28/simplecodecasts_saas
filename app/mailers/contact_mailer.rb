33b5572369f4f48bc6082412995ba8666c0d5eb1
Raul Barajas <rbarajas28@gmail.com>
2015-11-12 22:49:02 +0000

    Added ActionMailer settings

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-    default to: 'rbarajas@gmail.com'
+    default to: 'rbarajas28@gmail.com'
     
     def contact_email(name,email,body)
         @name = name
