75a587186f48b80ca9328e2c3ab0ecaf2f0c74e4
Raul Barajas <rbarajas28@gmail.com>
2015-11-25 16:59:27 +0000

    alert

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,6 +1,8 @@
 /* global Stripe*/
 $(document).ready(function()
 {
+  window.alert("Hello");
+  
   Stripe.setPublishableKey($('meta[name="stripe-key"]').attr('content'));
   
   // Watch for a form submission:
