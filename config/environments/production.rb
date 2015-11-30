869ff0aaf5ace47c1a55f16a9541b89642226f8d
Raul Barajas <rbarajas28@gmail.com>
2015-11-30 22:07:17 +0000

    Updated issues with heroku 5

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.assets.debug = true
+  config.assets.debug = false
 
   # Code is not reloaded between requests.
   config.cache_classes = false
