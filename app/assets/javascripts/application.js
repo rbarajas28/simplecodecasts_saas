209b04f67a90d0cfd459f4a29d329aaa2632a891
Raul Barajas <rbarajas28@gmail.com>
2015-11-30 22:10:54 +0000

    Updated issues with heroku 7

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,3 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-$(document).ready(function()
-{
-  window.alert("Main");
-});
