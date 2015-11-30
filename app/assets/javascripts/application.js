c76b5b94afc3368cd0d49d657e8dcc8e8ebbde83
Raul Barajas <rbarajas28@gmail.com>
2015-11-30 22:08:35 +0000

    Updated issues with heroku 6

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,10 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap-sprockets
 //= require jquery_ujs
-//= require_tree .
-//= require users
 $(document).ready(function()
 {
   window.alert("Main");
